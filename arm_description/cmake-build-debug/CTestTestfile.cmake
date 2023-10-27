650ef8738af8cc3ca9177c0b40b36933b54c565d
stefanocovone <stefanocovone@gmail.com>
2023-10-27 12:11:08 +0200

    cmake

---
+++ arm_description/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/stefanocovone/catkin_ws/src/homework1/arm_description
+# Build directory: /home/stefanocovone/catkin_ws/src/homework1/arm_description/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ arm_description/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/stefanocovone/catkin_ws/src/homework1/arm_description/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/stefanocovone/catkin_ws/src/homework1/arm_description/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/stefanocovone/catkin_ws/src/homework1/arm_description:/home/stefanocovone/catkin_ws/src/homework1/arm_description:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_control:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_description:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_gazebo:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_msgs:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_ros:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_hw:/home/stefanocovone/catkin_ws/src/iiwa_stack/iiwa_moveit:/home/stefanocovone/catkin_ws/src/my_package:/home/stefanocovone/catkin_ws/src/ros_tools:/home/stefanocovone/catkin_ws/src/ros_tutorials/ros_tutorials:/home/stefanocovone/catkin_ws/src/ros_tutorials/roscpp_tutorials:/home/stefanocovone/catkin_ws/src/ros_tutorials/rospy_tutorials:/home/stefanocovone/catkin_ws/src/ros_tutorials/turtlesim:/opt/ros/noetic/share'
