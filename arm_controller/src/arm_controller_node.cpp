f8f1519bf553c2600df340899936e8a05aef531f
stefanocovone <stefanocovone@gmail.com>
2023-10-28 13:12:35 +0200

    created  a subscriber to joint_states

--- arm_controller/src/arm_controller_node.cpp
+++ arm_controller/src/arm_controller_node.cpp
@@ -5,16 +5,40 @@
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr& msg) {
     // Your controller logic goes here
     // Publish control commands as needed
+    //ROS_INFO("Joint states: [%s]", msg->data.c_str());
+
+    // Print the current joint positions
+    ROS_INFO("\n--Joint States--\n");
+    for (size_t i = 0; i < msg->name.size(); i++) {
+        ROS_INFO("Joint %s: Position %.4f", msg->name[i].c_str(), msg->position[i]);
+    }
 }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "arm_controller_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber jointStateSub = nh.subscribe("/joint_states", 10, jointStateCallback);
+    ros::Subscriber jointStateSub = nh.subscribe("/arm/joint_states", 10, jointStateCallback);
     // Add publishers for control commands if needed
 
     ros::spin();
 
     return 0;
 }
+/*
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+void chatterCallback(const std_msgs::String& msg)
+{
+    ROS_INFO("I heard: [%s]", msg.data.c_str());
+}
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle nodeHandle;
+    ros::Subscriber subscriber = nodeHandle.subscribe("
+    chatter",10,chatterCallback);
+    ros::spin();
+    return 0;
+}
+*/
