a20c160edca6907249f80b97ca7847ff945e63ac
stefanocovone <stefanocovone@gmail.com>
2023-10-28 13:53:06 +0200

    implemented a publisher for each joint controller

--- arm_controller/src/arm_controller_node.cpp
+++ arm_controller/src/arm_controller_node.cpp
@@ -2,16 +2,24 @@
 #include "sensor_msgs/JointState.h"
 #include "std_msgs/Float64.h"
 
+ros::Publisher joint0CommandPub;
+ros::Publisher joint1CommandPub;
+ros::Publisher joint2CommandPub;
+ros::Publisher joint3CommandPub;
+
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr& msg) {
     // Your controller logic goes here
     // Publish control commands as needed
     //ROS_INFO("Joint states: [%s]", msg->data.c_str());
 
     // Print the current joint positions
-    ROS_INFO("\n--Joint States--\n");
+    ROS_INFO("\n--Joint States--");
     for (size_t i = 0; i < msg->name.size(); i++) {
         ROS_INFO("Joint %s: Position %.4f", msg->name[i].c_str(), msg->position[i]);
     }
+    std_msgs::Float64 commandMsg;
+    commandMsg.data = msg->position[0]+0.05; // Set the command value as needed
+    joint0CommandPub.publish(commandMsg); // Publish the command
 }
 
 int main(int argc, char** argv) {
@@ -20,6 +28,12 @@ int main(int argc, char** argv) {
 
     ros::Subscriber jointStateSub = nh.subscribe("/arm/joint_states", 10, jointStateCallback);
     // Add publishers for control commands if needed
+    // Define a publisher for the command topic of a joint controller
+    joint0CommandPub = nh.advertise<std_msgs::Float64>("arm/PositionJointInterface_J0_controller/command", 10);
+    joint1CommandPub = nh.advertise<std_msgs::Float64>("arm/PositionJointInterface_J1_controller/command", 10);
+    joint2CommandPub = nh.advertise<std_msgs::Float64>("arm/PositionJointInterface_J2_controller/command", 10);
+    joint3CommandPub = nh.advertise<std_msgs::Float64>("arm/PositionJointInterface_J3_controller/command", 10);
+
 
     ros::spin();
 
